a1f8a07a20a9fc5ca5aa84e78894c44e90764b41
marcello8080 <marcello8080>
2020-02-16 11:23:33 +0000

    widget functions added

--- js/shortcode.js
+++ js/shortcode.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function ($) {
     function whatsappValidation(fullTelephone, message, errorTarget) {
         if (message) {
             if (fullTelephone) {
-                whatsappErrorMessage("", errorTarget);
+                whatsappErrorMessage(" ", errorTarget);
                 return true;
             }
             else {
