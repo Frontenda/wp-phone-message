ae39c40be09a0570b1cbd1f916daffbfe4635cac
marcello8080 <marcello8080>
2020-02-16 09:11:44 +0000

    styling message form

--- views/shortcode-form.php
+++ views/shortcode-form.php
@@ -1,7 +1,7 @@
     <div class="whatsapp-form">
-        <div class="whatsapp-title"><?= get_option('wp-whatsapp-me-title'); ?></div>
+        <h3 class="whatsapp-title"><?= get_option('wp-whatsapp-me-title'); ?></h3>
         <p class="whatsapp-text"><?= get_option('wp-whatsapp-me-text'); ?></p>
-        <textarea id="wp-whatsapp-me-message" ></textarea>
+        <textarea id="wp-whatsapp-me-message" placeholder="Write a message..."></textarea>
         <input hidden="text" id="wp-whatsapp-me-full-phone-number" value="<?= get_option('wp-whatsapp-me-full-phone-number'); ?>" />
         <input type="submit" id="wp-whatsapp-me-button" value="<?= get_option('wp-whatsapp-me-button'); ?>" />
     </div>
