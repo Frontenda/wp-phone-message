bb9ccb02f04b8980e6a679460158ad7f60df0657
webmarcello8080 <webmarcello8080@gmail.com>
2020-05-11 09:06:28 +0100

    new model added

--- includes/WpPhoneMessageAdmin.php
+++ includes/WpPhoneMessageAdmin.php
@@ -3,10 +3,13 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
 
     class WpPhoneMessageAdmin {
 
+        private $model;
+
         public function __construct(){
             add_action( 'admin_menu', array( $this, 'adminMenu' ) );
             add_action( 'admin_post', array( $this, 'adminSave' ) );
             add_action( 'admin_enqueue_scripts', array( $this, 'adminStyle' ) );
+            $this->model = new WpPhoneMessageModel;
         }
 
         public function adminMenu() {
@@ -15,12 +18,12 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
                 'WP Phone Message',
                 'manage_options',
                 'wp-phone-message-admin',
-                array( $this, 'WhatappMeAdminPage' ),
+                array( $this, 'adminPage' ),
                 91
             );
         }
 
-        public function WhatappMeAdminPage() {
+        public function adminPage() {
             //show the form
             include_once( PLUGIN_WPM_PATH . 'views/admin-form.php' );
         }
@@ -28,60 +31,15 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
         public function adminSave(){
             // save data
             if( ( $_POST['wp-phone-message-phone-number'] ) && ( $_POST['wp-phone-message-phone-prefix'] ) ) {
-                $this->saveData();
-                update_option( 'wp-phone-message-form-message', 'Settings saved.');
+                $this->model->setData($_POST);
+                $this->model->setMessage('Settings saved.');
             }
             else{
-                $this->saveData();
-                update_option( 'wp-phone-message-form-message', 'International prefix and Whatsapp phone number are required.');
+                $this->model->setData($_POST);
+                $this->model->setMessage('International prefix and Whatsapp phone number are required.');
             }
 
             $this->adminRedirect();
-        }
-
-        private function saveData(){
-
-            $phone = sanitize_text_field( $_POST['wp-phone-message-phone-number'] );
-            $prefix = (int) str_replace(' ', '', sanitize_text_field( $_POST['wp-phone-message-phone-prefix'] ));
-            $title = sanitize_text_field( $_POST['wp-phone-message-title'] );
-            $text = sanitize_text_field( $_POST['wp-phone-message-text'] );
-            $button = sanitize_text_field( $_POST['wp-phone-message-button'] );
-            $textarea = sanitize_text_field( $_POST['wp-phone-message-textarea'] );
-            $name_place = sanitize_text_field( $_POST['wp-phone-message-name'] );
-            $name_active = sanitize_text_field( $_POST['wp-phone-message-name-active'] );
-            $name_mandatory = sanitize_text_field( $_POST['wp-phone-message-name-mandatory'] );
-            $address_place = sanitize_text_field( $_POST['wp-phone-message-address'] );
-            $address_active = sanitize_text_field( $_POST['wp-phone-message-address-active'] );
-            $address_mandatory = sanitize_text_field( $_POST['wp-phone-message-address-mandatory'] );
-            $phone_place = sanitize_text_field( $_POST['wp-phone-message-phone'] );
-            $phone_active = sanitize_text_field( $_POST['wp-phone-message-phone-active'] );
-            $phone_mandatory = sanitize_text_field( $_POST['wp-phone-message-phone-mandatory'] );
-            $email_place = sanitize_text_field( $_POST['wp-phone-message-email'] );
-            $email_active = sanitize_text_field( $_POST['wp-phone-message-email-active'] );
-            $email_mandatory = sanitize_text_field( $_POST['wp-phone-message-email-mandatory'] );
-
-            $fullPhoneNumber = (int) str_replace(' ', '', $prefix) . ltrim(str_replace(' ', '', $phone), '0') ;
-
-            update_option( 'wp-phone-message-phone-number', $phone );
-            update_option( 'wp-phone-message-phone-prefix', $prefix );
-            update_option( 'wp-phone-message-full-phone-number', $fullPhoneNumber );
-            update_option( 'wp-phone-message-title', $title );
-            update_option( 'wp-phone-message-text', $text );
-            update_option( 'wp-phone-message-button', $button );
-            update_option( 'wp-phone-message-textarea', $textarea );
-            update_option( 'wp-phone-message-name', $name_place );
-            update_option( 'wp-phone-message-name-active', $name_active );
-            update_option( 'wp-phone-message-name-mandatory', $name_mandatory );
-            update_option( 'wp-phone-message-address', $address_place );
-            update_option( 'wp-phone-message-address-active', $address_active );
-            update_option( 'wp-phone-message-address-mandatory', $address_mandatory );
-            update_option( 'wp-phone-message-phone', $phone_place );
-            update_option( 'wp-phone-message-phone-active', $phone_active );
-            update_option( 'wp-phone-message-phone-mandatory', $phone_mandatory );
-            update_option( 'wp-phone-message-email', $email_place );
-            update_option( 'wp-phone-message-email-active', $email_active );
-            update_option( 'wp-phone-message-email-mandatory', $email_mandatory );
-
         }
 
         private function adminRedirect() {
@@ -108,7 +66,7 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
             wp_enqueue_script('wp-phone-message-admin', PLUGIN_WPM_URL . 'js/admin.min.js', array( 'jquery' ), '1.0.0', true );
         }
 
-        public function adminCallback() { // Section Callback
+        private function adminCallback() { // Section Callback
             echo '<p>This section is part of WP Phone Message Plugin</p>';
         }
     }
--- views/shortcode-form.php
+++ views/shortcode-form.php
@@ -1,9 +1,9 @@
 <?php
 $shortcode_form = '<div class="whatapp-wrapper">';
 $shortcode_form .= '<form class="whatapp-form" id="whatapp-form">';
-
 $shortcode_form .= '<h3 class="whatapp-title">' .  get_option('wp-phone-message-title') . '</h3>';
 $shortcode_form .= '<p class="whatapp-text">' .  get_option('wp-phone-message-text') . '</p>';
+
 if(get_option('wp-phone-message-name-active')){
     $shortcode_form .= '<input type="text" class="wp-phone-message-name" id="wp-phone-message-name" placeholder="' . get_option('wp-phone-message-name') . '"  ' . get_option('wp-phone-message-name-mandatory') . ' />';
 }
